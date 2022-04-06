c7e745597d162d6ec97c5250c6e9828fe892dab2
jinn2u <bsybear623@gmail.com>
2022-04-06 09:56:16 +0900

    ✨ Feature: Editor mark up

--- components/TuiEditor/index.tsx
+++ components/TuiEditor/index.tsx
@@ -1,6 +1,7 @@
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { MutableRefObject, useRef } from 'react';
+import * as S from './style';
 import { Editor } from '@toast-ui/react-editor';
 
 export default function TuiEditor() {
@@ -15,7 +16,7 @@ export default function TuiEditor() {
   }
 
   return (
-    <>
+    <S.Wrapper>
       <Editor
         initialValue="write post"
         ref={editorRef as MutableRefObject<Editor>}
@@ -23,6 +24,6 @@ export default function TuiEditor() {
         previewStyle="vertical"
       />
       <div onClick={handleSubmit}>전송!</div>
-    </>
+    </S.Wrapper>
   );
 }
---
+++ components/TuiEditor/style.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  height: 100vh;
+
+  &:first-of-type {
+    height: calc(100vh - 64px);
+  }
+`;

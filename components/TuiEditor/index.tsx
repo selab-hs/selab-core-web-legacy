bfd3c0be9a781e893592cccb8f7f108d236e8cd7
jinn2u <bsybear623@gmail.com>
2022-04-06 09:54:20 +0900

    🚚 Move: TuiEditor 폴더 이동

--- components/TuiEditor/index.tsx
+++ components/TuiEditor/index.tsx
@@ -1,26 +1,26 @@
 import 'codemirror/lib/codemirror.css';
 import '@toast-ui/editor/dist/toastui-editor.css';
-import { Editor } from '@toast-ui/react-editor';
 import { MutableRefObject, useRef } from 'react';
+import { Editor } from '@toast-ui/react-editor';
 
-export default function SetTuiEditor() {
+export default function TuiEditor() {
   const editorRef = useRef<Editor>();
-  //뭐가 들어올지 몰라서 제네릭 안에 ??을 명시함
 
   function handleSubmit() {
     if (!editorRef.current) return;
 
-    // const editorData = editorRef.current.getInstance().getHtml();
+    const editorData = editorRef.current.getInstance().getHtml();
 
-    // console.log(editorData);
+    console.log(editorData);
   }
 
   return (
     <>
       <Editor
         initialValue="write post"
-        initialEditType="wysiwyg"
         ref={editorRef as MutableRefObject<Editor>}
+        height="100%"
+        previewStyle="vertical"
       />
       <div onClick={handleSubmit}>전송!</div>
     </>

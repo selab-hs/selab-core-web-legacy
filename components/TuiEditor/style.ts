696e33810eb74014030a30cdb62e8479dcfe3330
jinn2u <bsybear623@gmail.com>
2022-04-07 09:48:19 +0900

    :sparkles:  Feature: write page Title 마크업(#18)

--- components/TuiEditor/style.ts
+++ components/TuiEditor/style.ts
@@ -4,6 +4,22 @@ export const Wrapper = styled.div`
   height: 100vh;
 
   &:first-of-type {
-    height: calc(100vh - 64px);
+    height: calc(100vh - 64px - 5.7rem);
   }
+  @media ${({ theme: { mediaQuery } }) => mediaQuery.mobile} {
+    &:first-of-type {
+      height: calc(100vh - 64px - 4.7rem);
+    }
+  }
+`;
+export const Title = styled.input`
+  box-sizing: border-box;
+  width: 100%;
+  height: 5.7rem;
+  padding: 2rem 0rem 1rem 2rem;
+  font-size: 2.7rem;
+  @media ${({ theme: { mediaQuery } }) => mediaQuery.mobile} {
+    height: 4.7rem;
+    font-size: 1.7rem;
+  } ;
 `;

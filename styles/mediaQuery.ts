1c71875c6fbf9c1072b7f30828de4e126eb57ec9
minbr0ther <minbr0ther@naver.com>
2022-04-11 12:07:43 +0900

    :sparkles:  Feature: 헤더 반응형 마크업 & 스크롤 반응 & 메뉴 클릭시 클릭한…

--- styles/mediaQuery.ts
+++ styles/mediaQuery.ts
@@ -1,9 +1,9 @@
 export const deviceSize = {
-  mobile: 768,
+  // mobile: 768,
   tablet: 1023,
 };
 export const mediaQuery = {
-  mobile: `screen and (max-width: ${deviceSize.mobile}px)`,
-  tablet: `screen and (min-width: ${deviceSize.mobile + 1}px and max-width: ${deviceSize.tablet})`,
+  // mobile: `screen and (max-width: ${deviceSize.mobile}px)`,
+  // tablet: `screen and (max-width: ${deviceSize.tablet}px)`,
   laptop: `screen and (min-width: 1024px)`,
 };

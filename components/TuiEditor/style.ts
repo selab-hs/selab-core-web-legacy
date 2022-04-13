bc5922774cc39116d675b406280e1d57d5bb2f96
jinn2u <bsybear623@gmail.com>
2022-04-13 11:36:18 +0900

    :sparkles:  Feature: write page에서 overflow-y가 되는것을 방지한다(#26)

--- components/TuiEditor/style.ts
+++ components/TuiEditor/style.ts
@@ -1,14 +1,11 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  height: 100vh;
+  height: calc(100vh - 82px - 75px - 16px - 3px);
 
-  &:first-of-type {
-    height: calc(100vh - 64px - 5.7rem);
-  }
-  @media ${({ theme: { mediaQuery } }) => mediaQuery.mobile} {
+  @media ${({ theme: { mediaQuery } }) => mediaQuery.laptop} {
     &:first-of-type {
-      height: calc(100vh - 64px - 4.7rem);
+      height: calc(100vh - 64px - 4.7rem - 82px);
     }
   }
 `;
@@ -18,7 +15,7 @@ export const Title = styled.input`
   height: 5.7rem;
   padding: 2rem 0rem 1rem 2rem;
   font-size: 2.7rem;
-  @media ${({ theme: { mediaQuery } }) => mediaQuery.mobile} {
+  @media ${({ theme: { mediaQuery } }) => mediaQuery.laptop} {
     height: 4.7rem;
     font-size: 1.7rem;
   } ;

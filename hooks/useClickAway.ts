2c8b71a69875b562219c40cc70de4fecf58565e7
jinn2u <bsybear623@gmail.com>
2022-04-22 08:53:48 +0900

    :sparkles:  Feature: passive:false가 default이기 때문에 삭제 (#14)

--- hooks/useClickAway.ts
+++ hooks/useClickAway.ts
@@ -2,18 +2,18 @@ import { useEffect, useRef } from 'react';
 
 export const useClickAway = <T>(awayEvent: () => void, deps?: T[]) => {
   const ref = useRef<HTMLElement>(null);
+
   const callback = (e: MouseEvent) => {
     const element = ref.current;
-    if (!element) {
-      return;
-    }
+    if (!element) return;
+
     if (!element.contains(e.target as Node)) {
       awayEvent();
     }
   };
   useEffect(
     () => {
-      document.body.addEventListener('click', callback, { passive: false });
+      document.body.addEventListener('click', callback);
       return () => document.body.removeEventListener('click', callback);
     },
     deps ? [...deps] : [],

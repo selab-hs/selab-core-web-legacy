ade4eb3fb7ea101e6b6884eda64373f62937857f
minbr0ther <minbr0ther@naver.com>
2022-04-22 10:17:09 +0900

    :sparkles:  Feature: 자유게시물 api통해서 불러오기(#4)

--- components/utils/timeForToday.ts
+++ components/utils/timeForToday.ts
@@ -1,4 +1,4 @@
-export const timeForToday = (value: string) => {
+export const timeForToday = (value: Date) => {
   const today = new Date();
   const timeValue = new Date(value);
 
---
+++ pages/free-posts/types.ts
@@ -0,0 +1,8 @@
+export interface FreePostType {
+  memberId: number;
+  freePostId: number;
+  title: string;
+  content: string;
+  createdAt: Date;
+  modifiedAt: Date;
+}

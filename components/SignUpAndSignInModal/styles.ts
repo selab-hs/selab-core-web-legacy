5de20f680cdde39176e621d43db3face7ec28fc1
jinn2u <bsybear623@gmail.com>
2022-04-24 22:54:37 +0900

    :sparkles:  Feature: Modal scroll overflow시 스크롤안보이게 하기(#14)

--- components/SignUpAndSignInModal/styles.ts
+++ components/SignUpAndSignInModal/styles.ts
@@ -1,4 +1,16 @@
 import styled from '@emotion/styled';
+import Modal from '../Modal';
+
+export const CuModal = styled(Modal)`
+  max-height: calc(100vh - 150px);
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 export const CloseBtn = styled.button`
   margin: 0 0 20px 100%;
@@ -6,6 +18,7 @@ export const CloseBtn = styled.button`
   color: lightgray;
 `;
 export const Form = styled.form`
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
 `;
@@ -21,11 +34,26 @@ export const Input = styled.input`
   }
 `;
 export const Label = styled.label`
-  margin-bottom: 11px;
+  margin: 11px 0;
   font-weight: 400;
   color: ${({ theme: { colors } }) => `${colors.gray100}`};
 `;
 export const ErrorMsg = styled.p`
   margin-top: 11px;
   color: ${({ theme: { colors } }) => `${colors.red}`};
+`;
+
+export const Title = styled.h1`
+  display: flex;
+  justify-content: center;
+  font-weight: 600;
+`;
+
+export const SubmitBtn = styled.button`
+  width: 360px;
+  height: 50px;
+  margin-top: 20px;
+  color: white;
+  background-color: ${({ theme: { colors } }) => `${colors.blue}`};
+  border-radius: 5px;
 `;

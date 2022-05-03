a20c4e7e9ed4c8ba7dc24f6d3578b8f4ae035552
minbr0ther <minbr0ther@naver.com>
2022-05-03 09:47:03 +0900

    :art:  Style: 글로벌 스타일 파일 merging(#43)

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -2,6 +2,223 @@ import { css, Theme } from '@emotion/react';
 
 export const GlobalStyle = (props: Theme) =>
   css`
+    *,
+    ::before,
+    ::after {
+      box-sizing: border-box;
+      border-color: theme('borderColor.DEFAULT', 'currentColor');
+      border-style: solid;
+      border-width: 0;
+    }
+
+    ::before,
+    ::after {
+      --tw-content: '';
+    }
+
+    hr {
+      height: 0;
+      color: inherit;
+      border-top-width: 1px;
+    }
+
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      font-size: inherit;
+      font-weight: inherit;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: inherit;
+      cursor: pointer;
+      outline: none;
+    }
+
+    b,
+    strong {
+      font-weight: bolder;
+    }
+
+    code,
+    kbd,
+    samp,
+    pre {
+      font-family: theme(
+        'fontFamily.mono',
+        ui-monospace,
+        SFMono-Regular,
+        Menlo,
+        Monaco,
+        Consolas,
+        'Liberation Mono',
+        'Courier New',
+        monospace
+      );
+      font-size: 1em;
+    }
+
+    small {
+      font-size: 80%;
+    }
+
+    sub,
+    sup {
+      position: relative;
+      font-size: 75%;
+      line-height: 0;
+      vertical-align: baseline;
+    }
+
+    sub {
+      bottom: -0.25em;
+    }
+
+    sup {
+      top: -0.5em;
+    }
+
+    table {
+      text-indent: 0;
+      border-spacing: 0;
+      border-collapse: collapse;
+      border-color: inherit;
+    }
+
+    button,
+    input,
+    optgroup,
+    select,
+    textarea {
+      padding: 0;
+      margin: 0;
+      font-family: inherit;
+      font-size: 100%;
+      line-height: inherit;
+      color: inherit;
+    }
+
+    button,
+    select {
+      text-transform: none;
+    }
+
+    button,
+    [type='button'],
+    [type='reset'],
+    [type='submit'] {
+      appearance: button;
+      background-color: transparent;
+      background-image: none;
+    }
+
+    :-moz-focusring {
+      outline: auto;
+    }
+
+    :-moz-ui-invalid {
+      box-shadow: none;
+    }
+
+    progress {
+      vertical-align: baseline;
+    }
+
+    ::-webkit-inner-spin-button,
+    ::-webkit-outer-spin-button {
+      height: auto;
+    }
+
+    [type='search'] {
+      appearance: textfield;
+      outline-offset: -2px;
+    }
+
+    ::-webkit-search-decoration {
+      appearance: none;
+    }
+
+    ::-webkit-file-upload-button {
+      appearance: button;
+      font: inherit;
+    }
+
+    summary {
+      display: list-item;
+    }
+
+    blockquote,
+    dl,
+    dd,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    hr,
+    figure,
+    p,
+    pre {
+      margin: 0;
+    }
+
+    fieldset {
+      padding: 0;
+      margin: 0;
+    }
+
+    legend {
+      padding: 0;
+    }
+
+    ol,
+    ul,
+    menu {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+    }
+
+    textarea {
+      resize: vertical;
+    }
+
+    button,
+    [role='button'] {
+      cursor: pointer;
+    }
+
+    :disabled {
+      cursor: default;
+    }
+
+    img,
+    svg,
+    video,
+    canvas,
+    audio,
+    iframe,
+    embed,
+    object {
+      display: block;
+      vertical-align: middle;
+    }
+
+    img,
+    video {
+      max-width: 100%;
+      height: auto;
+    }
+
+    [hidden] {
+      display: none;
+    }
+
     html,
     body,
     div,
@@ -124,17 +341,6 @@ export const GlobalStyle = (props: Theme) =>
       content: none;
     }
 
-    table {
-      border-spacing: 0;
-      border-collapse: collapse;
-    }
-
-    a {
-      text-decoration: none;
-      cursor: pointer;
-      outline: none;
-    }
-
     button {
       padding: 0;
       overflow: visible;
@@ -172,10 +378,11 @@ export const GlobalStyle = (props: Theme) =>
     }
 
     body {
+      margin: 0;
       font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
         'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic',
         sans-serif;
-      line-height: 1;
+      line-height: inherit;
       color: ${props.colors.black};
       background: ${props.colors.background};
     }

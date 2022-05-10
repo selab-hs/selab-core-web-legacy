521594be1f10aefdeed552d6431bb10b8c520a5e
jinn2u <bsybear623@gmail.com>
2022-05-10 21:54:37 +0900

    :recycle:  refactor: useDebounce를 활용하여 title과 content에 debounce 적용

--- domains/TuiEditor/index.tsx
+++ domains/TuiEditor/index.tsx
@@ -19,58 +19,63 @@ import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 
-import { MutableRefObject, useEffect, useRef } from 'react';
-import { v4 } from 'uuid';
+import { MutableRefObject, useRef } from 'react';
 
 import { useGetWindowSize } from '@hooks/useGetWindowSize';
 import * as S from './style';
 import { storage } from '@components/utils';
-import { TEMPORARY_POSTS } from '@components/utils/constants';
 import { deviceSize } from '@styles/theme/mediaQuery';
 import { useEditorDarkMode } from './useEditorDarkMode';
+import { useDebounce } from '@hooks/useDebounce';
+import { TEMPORARY_POST } from '@constants/posts-constants';
+import { v4 } from 'uuid';
 
 const TuiEditor = () => {
   const titleRef = useRef<HTMLInputElement>(null);
-  const editorRef = useRef<Editor>();
-  const timeoutId = useRef(0);
+  const editorRef = useRef<Editor>(null);
 
   const windowSize = useGetWindowSize();
   useEditorDarkMode();
-  const handleEditorChange = () => {
-    if (timeoutId.current) {
-      clearTimeout(timeoutId.current);
-    }
-    timeoutId.current = window.setTimeout(() => {
-      if (!editorRef.current) return;
-      const content = editorRef.current.getInstance().getMarkdown();
+  // const handleEditorChange = onEditorChange(editorRef, titleRef);
 
-      const postObj = [
-        {
-          id: v4(),
-          title: titleRef.current?.value || '',
-          content,
-        },
-      ];
+  // useEffect(() => {
+  //   if (!editorRef.current || !titleRef.current) return;
+  //   // TODO: 데이터 추가되면 변경해야함
 
-      storage.set(TEMPORARY_POSTS, postObj);
-    }, 500);
-  };
+  //   const tmpPost = storage.get<{ id: string; title: string; content: string }[]>(TEMPORARY_POSTS);
+  //   const content = tmpPost ? tmpPost[0].content : '';
+  //   const title = tmpPost ? tmpPost[0].title : '';
 
-  useEffect(() => {
-    if (!editorRef.current || !titleRef.current) return;
-    // TODO: 데이터 추가되면 변경해야함
+  //   titleRef.current.value = title;
+  //   editorRef.current.getInstance().setMarkdown(content);
+  // }, []);
 
-    const tmpPost = storage.get<{ id: string; title: string; content: string }[]>(TEMPORARY_POSTS);
-    const content = tmpPost ? tmpPost[0].content : '';
-    const title = tmpPost ? tmpPost[0].title : '';
+  const handleTitleChange = useDebounce(() => {
+    const title = titleRef?.current?.value || '';
+    const content = editorRef?.current?.getInstance().getMarkdown() || '';
+    if (!title && !content) return;
 
-    titleRef.current.value = title;
-    editorRef.current.getInstance().setMarkdown(content);
-  }, []);
+    const nextPost = { id: v4(), title, content };
+    storage.set(TEMPORARY_POST, nextPost);
+  }, 500);
+
+  const handleEditorChange = useDebounce(() => {
+    const title = titleRef?.current?.value || '';
+    const content = editorRef?.current?.getInstance().getMarkdown() || '';
+    if (!title && !content) return;
+
+    const postObj = {
+      id: v4(),
+      title,
+      content,
+    };
+
+    storage.set(TEMPORARY_POST, postObj);
+  }, 500);
 
   return (
     <S.Wrapper>
-      <S.Title placeholder="제목을 입력하세요" ref={titleRef} />
+      <S.Title placeholder="제목을 입력하세요" ref={titleRef} onChange={handleTitleChange} />
       <Editor
         ref={editorRef as MutableRefObject<Editor>}
         height="100%"

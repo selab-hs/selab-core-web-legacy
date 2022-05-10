7494e497b28db11dfd7f1fa48de3b4abb538e9ba
jinn2u <bsybear623@gmail.com>
2022-05-10 14:43:36 +0900

    :recycle:  refactor: DarkMode 초기값을 storage에서받아오도록 변경

--- stores/modules/darkModes/index.tsx
+++ stores/modules/darkModes/index.tsx
@@ -1,14 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
+
+import { storage } from '@components/utils';
+import { DARK_MODE_KEY, DARK_MODE_VALUE } from '@constants/theme-constants';
 import { DarkModesState } from './types';
 
 export const darkModes = createSlice({
   name: 'darkModes',
   initialState: {
-    mode: 'light',
+    mode: storage.get<Pick<DarkModesState, 'mode'>>(DARK_MODE_KEY) || DARK_MODE_VALUE.LIGHT,
   } as DarkModesState,
   reducers: {
     toggleDarkModes: (state: DarkModesState) => {
-      state.mode = state.mode === 'light' ? 'dark' : 'light';
+      state.mode =
+        state.mode === DARK_MODE_VALUE.LIGHT ? DARK_MODE_VALUE.DARK : DARK_MODE_VALUE.LIGHT;
     },
   },
 });
